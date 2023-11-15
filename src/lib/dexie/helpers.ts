0ba6bc64799dbfd85234516a893d195002e3f1ad
Colin Chung <colin.d.chung@gmail.com>
2023-11-14 21:54:00 -0500

    signout

--- src/lib/dexie/helpers.ts
+++ src/lib/dexie/helpers.ts
@@ -1,5 +1,6 @@
 import { db } from "../db";
 import { isOutlookNextPageTokenNewer } from "../../api/outlook/helpers";
+import { dLog } from "../noProd";
 
 export async function getGoogleMetaData(email: string, folderId: string) {
   const metaData = await db.googleMetadata.where("email").equals(email).first();
@@ -93,3 +94,39 @@ export async function setHistoryId(
       });
   }
 }
+
+export const clearEmailFromDb = async (email: string) => {
+  try {
+    const emails = (await db.emails.toArray()).filter((e) => e.email !== email);
+
+    if (emails.length === 0) {
+      // Clear db if this is the only remaining email
+      db.tables.forEach((table) => void table.clear());
+    } else {
+      const selectedEmail = await db.selectedEmail.get(1);
+      const emailThreads = (
+        await db.emailThreads.where("email").equals(email).toArray()
+      ).map((t) => t.id);
+
+      void db.emails.where("email").equals(email).delete();
+      void db.emailThreads.where("id").anyOf(emailThreads).delete();
+      void db.messages.where("threadId").anyOf(emailThreads).delete();
+      void db.googleMetadata.where("email").equals(email).delete();
+      void db.outlookMetadata.where("email").equals(email).delete();
+
+      // If the selected email is being deleted, select the first remaining email
+      if (selectedEmail?.email === email) {
+        void db.selectedEmail.put({
+          id: 1,
+          email: emails[0].email,
+          provider: emails[0].provider,
+        });
+      }
+    }
+  } catch (e) {
+    dLog(e);
+    return e;
+  }
+
+  return null;
+};
