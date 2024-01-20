012ddfcd262bce9536f076cb29ec37fe3a280446
Colin Chung <colin.d.chung@gmail.com>
2024-01-20 12:27:39 -0500

    forgot to push deleted

--- src/pages/DeletedItems.tsx
+++ src/pages/DeletedItems.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ThreadView from "../components/ThreadViews/ThreadView";
 import { FOLDER_IDS } from "../api/constants";
-import { GMAIL_FOLDER_IDS_MAP } from "../api/gmail/constants";
-import { OUTLOOK_FOLDER_IDS_MAP } from "../api/outlook/constants";
+// import { GMAIL_FOLDER_IDS_MAP } from "../api/gmail/constants";
+// import { OUTLOOK_FOLDER_IDS_MAP } from "../api/outlook/constants";
 import { ISelectedEmail, db } from "../lib/db";
 import Titlebar from "../components/Titlebar";
 import { useEmailPageOutletContext } from "./_emailPage";
 import { useQuery } from "react-query";
 import { getThreadsExhaustive } from "../api/gmail/reactQuery/reactQueryFunctions";
 
-const gmailFetchQuery = `&labelIds=${GMAIL_FOLDER_IDS_MAP.getValue(
-  FOLDER_IDS.TRASH
-)}&includeSpamTrash=true`;
-const outlookFetchQuery = `mailFolders/${OUTLOOK_FOLDER_IDS_MAP.getValue(
-  FOLDER_IDS.TRASH
-)}/messages?$select=id,conversationId,createdDateTime&$top=20`;
+// const gmailFetchQuery = `&labelIds=${GMAIL_FOLDER_IDS_MAP.getValue(
+//   FOLDER_IDS.TRASH
+// )}&includeSpamTrash=true`;
+// const outlookFetchQuery = `mailFolders/${OUTLOOK_FOLDER_IDS_MAP.getValue(
+//   FOLDER_IDS.TRASH
+// )}/messages?$select=id,conversationId,createdDateTime&$top=20`;
 
 const filterThreadsFnc = (selectedEmail: ISelectedEmail) =>
   db.emailThreads
