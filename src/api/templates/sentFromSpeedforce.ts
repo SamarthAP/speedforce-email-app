483bc86b97e90a998c57e67e266106392450dde2
Colin Chung <colin.d.chung@gmail.com>
2024-03-09 16:47:07 -0500

    sent from speed with beta campaign

---
+++ src/api/templates/sentFromSpeedforce.ts
@@ -0,0 +1,5 @@
+export const SENT_FROM_SPEEDFORCE_HTML = `
+  <div style="font-style: italic;">
+    Sent from <a href="https://speedforce.me?utm_source=email_client&utm_medium=email_signature&utm_campaign=beta" target="_blank" rel="noopener noreferrer">Speedforce</a>
+  </div>
+`;
