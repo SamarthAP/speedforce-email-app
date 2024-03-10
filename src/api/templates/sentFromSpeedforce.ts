01b06a527056efad49b2d1ea740e82e5420131c0
Samarth Patel <samarth.patel.22@hotmail.com>
2024-03-10 10:37:01 -0700

    from -> via

--- src/api/templates/sentFromSpeedforce.ts
+++ src/api/templates/sentFromSpeedforce.ts
@@ -1,5 +1,5 @@
 export const SENT_FROM_SPEEDFORCE_HTML = `
   <div style="font-style: italic;">
-    Sent from <a href="https://speedforce.me?utm_source=email_client&utm_medium=email_signature&utm_campaign=beta" target="_blank" rel="noopener noreferrer">Speedforce</a>
+    Sent via <a href="https://speedforce.me?utm_source=email_client&utm_medium=email_signature&utm_campaign=beta" target="_blank" rel="noopener noreferrer">Speedforce</a>
   </div>
 `;
